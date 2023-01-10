93ba2e8173daade36bc63a84676908d20dd57af3
Nicolas DELAUNEY <niko-d@live.fr>
2023-01-10 22:16:20 +0100

    ✨ transition between pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { fly } from 'svelte/transition';
+  import Salary from './lib/Salary.svelte'
+  import Target from './lib/Target.svelte'
+  import Result from './lib/Result.svelte'
+
+  let page = 0
 </script>
 
 <main>
+  {#if page == 0 }
+  <div class="page" in:fly={{x: 1000 }} out:fly={{x: -1000 }}><Salary /></div>
+  {:else if page == 1 }
+  <div class="page" in:fly={{x: 1000 }} out:fly={{x: -1000 }}><Target /></div>
+  {:else }
+  <div class="page" in:fly={{x: 1000 }} out:fly={{x: -1000 }}><Result /></div>
+  {/if}
   
 </main>
 
-<style>
-
+<style lang=sass>
+.page
+  position: fixed
+  top: 0
+  left: 0
+  width: 100vw
+  // height: 100vh
 </style>
