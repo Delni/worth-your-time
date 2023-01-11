569e53e8128caa7d51957baaa0ad4412e4df9e82
Nicolas DELAUNEY <niko-d@live.fr>
2023-01-11 09:29:35 +0100

    💄 improve page transitions

--- src/App.svelte
+++ src/App.svelte
@@ -6,18 +6,46 @@
   import Result from './lib/Result.svelte'
 
   let page = 0
-  const nextPage = () => page = (page + 1) % 4
+  let motion = 1
+  const nextPage = () => {
+    motion = 1
+    if(page + 1 >= 4) {
+      stepBackwards(3)
+    } else {
+      page++
+    }
+  }
+
+  const previousPage = () => {
+    motion = -1
+    page--
+  }
+
+  const stepBackwards = (times: number) => {
+    for (let index = 0; index < times; index++) {
+      setTimeout(previousPage, 200 * index)
+    }
+  }
+
 </script>
 
 <main>
   {#if page == 0 }
-  <div class="page" in:fly={{x: 1000 }} out:fly={{x: -1000 }}><Introduction on:click={nextPage}/></div>
+    <div class="page" in:fly={{x: 250 * motion }} out:fly={{x: -250 * motion }}>
+      <Introduction on:click={nextPage}/>
+    </div>
   {:else if page == 1 }
-  <div class="page" in:fly={{x: 1000 }} out:fly={{x: -1000 }}><Salary on:click={nextPage}/></div>
+    <div class="page" in:fly={{x: 250 * motion }} out:fly={{x: -250 * motion }}>
+      <Salary on:next={nextPage} on:previous={previousPage}/>
+    </div>
   {:else if page == 2 }
-  <div class="page" in:fly={{x: 1000 }} out:fly={{x: -1000 }}><Target on:click={nextPage}/></div>
+    <div class="page" in:fly={{x: 250 * motion }} out:fly={{x: -250 * motion }}>
+      <Target on:click={nextPage}/>
+    </div>
   {:else }
-  <div class="page" in:fly={{x: 1000 }} out:fly={{x: -1000 }}><Result on:click={nextPage}/></div>
+    <div class="page" in:fly={{x: 250 * motion }} out:fly={{x: -250 * motion }}>
+      <Result on:click={nextPage}/>
+    </div>
   {/if}
   
 </main>
@@ -28,5 +56,4 @@
   top: 0
   left: 0
   width: 100vw
-  // height: 100vh
 </style>
