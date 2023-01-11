bf4d1f71bc49a275547323de8a08919361854905
Nicolas DELAUNEY <niko-d@live.fr>
2023-01-11 09:29:25 +0100

    ✨ Add previous and next events

--- src/lib/Salary.svelte
+++ src/lib/Salary.svelte
@@ -1,2 +1,60 @@
+<article>
+	<h2>How much do you earn ?</h2>
+	<small>Money is just the common value of your time.</small>
 
-<h2>Page 1</h2>
+	<div class="input-group">
+		<label for="weekly-income">Weekly income</label>
+		<input type="number" name="weekly-income" id="weekly-income" bind:value={weekly} on:change={inferYM} on:input={inferYM}/>
+	</div>
+	<div class="input-group">
+		<label for="monthly-income">Monthly income</label>
+		<input type="number" name="monthly-income" id="monthly-income" bind:value={monthly} on:change={inferYW} on:input={inferYW}/>
+	</div>
+	<div class="input-group">
+		<label for="yearly-income">Yearly income</label>
+		<input type="number" name="yearly-income" id="yearly-income" bind:value={yearly} on:change={inferMW} on:input={inferMW}/>
+	</div>
+	<ButtonBar on:next on:previous/>
+</article>
+
+
+<script lang="ts">
+	import ButtonBar from './components/ButtonBar.svelte';
+	let weekly: number = 0
+	let monthly: number = 0
+	let yearly: number = 0
+
+	const inferYM = () => {
+		monthly = weekly * 4
+		yearly = monthly * 12
+	}
+
+	const inferYW = () => {
+		weekly = monthly / 4
+		yearly = monthly * 12
+	}
+
+	const inferMW = () => {
+		monthly = yearly / 12
+		weekly = monthly / 4
+	}
+</script>
+
+
+<style lang="sass">
+article
+	display: flex
+	flex-flow: column nowrap
+	max-width: 500px
+	padding: 2rem
+	margin: auto
+	gap: 1rem
+	h2
+		margin-bottom: 0
+	small
+		margin-bottom: 2rem
+	.input-group
+		display: flex
+		flex-flow: row nowrap
+		justify-content: space-between
+</style>
---
+++ src/lib/components/ButtonBar.svelte
@@ -0,0 +1,20 @@
+<div class="button-bar">
+	<button on:click={previous}>Back</button>
+	<button on:click={next}>Forward</button>
+</div>
+
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
+	const dispatch = createEventDispatcher();
+	const previous = () => dispatch("previous")
+	const next = () => dispatch("next")
+</script>
+
+<style lang="sass">
+.button-bar
+	display: flex
+	flex-flow: row nowrap
+	justify-content: space-between
+	margin-top: 2rem
+</style>
