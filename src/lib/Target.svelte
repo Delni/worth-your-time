e6d125e147c61c1cf3444c624e436df3112f9ffd
Nicolas DELAUNEY <niko-d@live.fr>
2023-01-11 17:33:36 +0100

    ✨ Add target page

--- src/lib/Target.svelte
+++ src/lib/Target.svelte
@@ -1,2 +1,12 @@
-<h2>Page 2</h2>
-<button on:click>Next</button>
+<script>
+    import ButtonBar from './components/ButtonBar.svelte'
+	import { prompt } from './stores/input.store'
+</script>
+
+<section>
+	<h2>What is the cost of that thing you want?</h2>
+	<small>They tell you a price so you can't put a value on it</small>
+
+	<input type="number" name="cost" id="cost" bind:value={$prompt.target}>
+	<ButtonBar on:next on:previous/>
+</section>
