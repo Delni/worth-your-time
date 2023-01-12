5750ab75c2f714d27d433b783b3155a0aa5453fd
Nicolas DELAUNEY <niko-d@live.fr>
2023-01-12 11:20:09 +0100

    🐛 rewrite delays

--- src/lib/Result.svelte
+++ src/lib/Result.svelte
@@ -5,33 +5,40 @@
   	import { circOut } from "svelte/easing";
     import ButtonBar from './components/ButtonBar.svelte'
 
-
+	const finalValue = $prompt.target / ($prompt.weekly || 1)
+	const duration = 3000
 	let percent = tweened(0, {
-		duration: 3000,
+		duration,
 		easing: circOut,
-		interpolate: (from, to) => (t) => Math.round(to*t),
+		interpolate: (_, to) => (t) => Math.round(to*t),
 	});
-	$: $percent = Math.round($prompt.target / ($prompt.weekly || 1) * 100)
+	$: $percent = Math.round(finalValue * 100)
 
-	$: years = Math.floor($percent / 100 / 52)
-	$: weeks = Math.floor($percent / 100 - years * 52)
-	$: days = Math.floor(($percent / 100 * 7 - (years * 52 + weeks) * 7)*10)/10
+	
+	const years = Math.floor(finalValue / 52)
+	const weeks = Math.floor(finalValue - years * 52)
+	const days = Math.floor((finalValue * 7 - (years * 52 + weeks) * 7)*100)/100
+
+	const yearDelay = duration + 500 * (years / (years || 1))
+	const weekDelay = yearDelay + 500 * (weeks / (weeks || 1))
+	const dayDelay = weekDelay + 500 * (days / (days || 1))
+	const endDelay = dayDelay + 500
 </script>
 
 <h2>Here you go</h2>
 <p>You seek something worth <strong>{$percent}%</strong> of your weekly salary</p>
-<p in:fade={{delay: 3000}}>
-	<span in:fade={{delay:3000}}>That is to say working for</span>
-	<span in:fade={{delay:3500}}> {#if years != 0}<strong>{years}</strong> years{/if}</span>
-	<span in:fade={{delay:4000}}>{#if weeks != 0}<strong>{weeks}</strong> weeks{/if}</span>
-	<span in:fade={{delay:4500}}>
+<h3>
+	<span in:fade={{delay:duration}}>That is to say working for</span>
+	<span in:fade={{delay:yearDelay}}> {#if years != 0}<strong>{years}</strong> years{/if}</span>
+	<span in:fade={{delay:weekDelay}}>{#if weeks != 0}<strong>{weeks}</strong> weeks{/if}</span>
+	<span in:fade={{delay:dayDelay}}>
 		{#if weeks + years != 0 && days != 0} and{/if}
 		{#if days != 0} <strong>{days}</strong> days{/if}
 	</span>
-	<span in:fade={{delay:4750}}>straight.</span>
-</p>
-<p in:fade={{delay: 5500}}>Do you still think it's worth it ?</p>
-<small in:fade={{delay: 6000}}>Notice that we never asked you your currency. It is not relevant.</small>
+	<span in:fade={{delay:endDelay}}>straight.</span>
+</h3>
+<p in:fade={{delay: endDelay + 500}}>Do you still think it's worth it ?</p>
+<small in:fade={{delay: endDelay + 1000}}>Notice that we never asked you your currency. It is not relevant.</small>
 <ButtonBar on:next on:previous>
 	<span slot="next-label">Restart</span>
 </ButtonBar>
